99e18c31f0856a48088f61ea69183ec8b6ea54ba
hotneure <hotneure@163.com>
2018-01-21 00:38:40 +0800

    增加电压检测

--- HardWare/pid.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PID_H
-#define _PID_H
-
-void PID(signed char error_value);
-void PID_Circulation(float NewCurrent, float LastCirCurrent);
-
-#endif
--- HardWare/tim2.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TIM2_H
-#define _TIM2_H
-
-void TIM2_Init(void);
-void GPIO_Convert(void);
-void TIM2_Clr(void);
-void CLK_Configuration(void);
-#endif
